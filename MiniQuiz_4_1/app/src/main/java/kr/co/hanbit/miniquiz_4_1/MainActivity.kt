d875a7c51378d1dc60e718ac8dc7fed2cedfcc0a
javafa <nigne2016@gmail.com>
2020-04-07 13:58:45 +0900

    added MiniQuiz4_1

---
+++ MiniQuiz_4_1/app/src/main/java/kr/co/hanbit/miniquiz_4_1/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.hanbit.miniquiz_4_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
