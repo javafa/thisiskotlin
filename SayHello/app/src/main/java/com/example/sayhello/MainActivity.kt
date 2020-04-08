3c7d0ea5e46e3422170375a7e34eb08bdeaa1a61
javafa <nigne2016@gmail.com>
2020-04-08 18:17:47 +0900

    updated sayhello

--- SayHello/app/src/main/java/com/example/sayhello/MainActivity.kt
+++ SayHello/app/src/main/java/com/example/sayhello/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.sayhello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnSay.setOnClickListener{
+            textSay.setText("Hello Kotlin!!!")
+        }
     }
 }
