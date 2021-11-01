6a4fbe0b913fe0a926e1a0a2709e7d7739814230
Javafa <nigne2016@gmail.com>
2021-11-01 21:30:56 +0900

    3rd edition

--- SQLite/app/src/main/java/com/example/sqlite/SqliteHelper.kt
+++ SQLite/app/src/main/java/com/example/sqlite/SqliteHelper.kt
@@ -1,5 +1,6 @@
 package com.example.sqlite
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
@@ -37,13 +38,18 @@ class SqliteHelper(context: Context, name: String, version: Int) :
         val rd = readableDatabase
         val cursor = rd.rawQuery(select, null)
         while (cursor.moveToNext()) {
-            val no = cursor.getLong(cursor.getColumnIndex("no"))
-            val content = cursor.getString(cursor.getColumnIndex("content"))
-            val datetime = cursor.getLong(cursor.getColumnIndex("datetime"))
+            // 먼저 컬럼의 이름으로 각각의 위치를 가져온다.
+            val noIdx = cursor.getColumnIndex("no") // 1 : 테이블에서 no 컬럼의 순서
+            val contentIdx = cursor.getColumnIndex("content") // 2
+            val dateIdx = cursor.getColumnIndex("datetime") // 3
+
+            val no = cursor.getLong(noIdx) // 값은 위에서 저장해 둔 컬럼의 위치로 가져온다
+            val content = cursor.getString(contentIdx)
+            val datetime = cursor.getLong(dateIdx)
 
             list.add(Memo(no, content, datetime))
         }
-
+        
         cursor.close()
         rd.close()
         return list
